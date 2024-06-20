78ebaece562dc4c6d3a9eaa4733c154238128d1b
Timoshenko Dmitry <dima00138@gmail.com>
2024-06-20 20:53:19 +0300

    complete data-types

---
+++ data-types/task-1.js
@@ -0,0 +1,18 @@
+function objectsAreEqual(obj1, obj2) {
+    if (obj1 === null && obj2 === null) return true;
+    if ((obj1 === null) || (obj2 === null)) return false;
+
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
+    for (let key in obj1) {
+        if (!(key in obj2)) return false;
+        if (obj1[key] === obj2[key]) continue;
+        else return false;
+    }
+
+    return true;
+}
+
+const date = new Date(11);
+console.log(objectsAreEqual({date: new Date(11)}, {date: new Date(11)}));
+console.log(objectsAreEqual({date: date}, {date: date}));
